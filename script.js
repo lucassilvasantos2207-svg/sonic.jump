e863638669c8bf6b5fe8c4337172377062766a39
Lucas Silva <lucassilvasantos2207@gmail.com>
2026-05-23 13:55:10 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+const sonic = document.querySelector('.sonic');
+const espinho = document.querySelector('.espinho');
+
+const jump = () => {
+
+    sonic.classList.add('jump');
+
+    setTimeout(() => {
+
+        sonic.classList.remove('jump');
+
+    }, 500);
+};
+
+const loop = setInterval(() => {
+
+    const espinhoPosition = espinho.offsetLeft;
+
+    const sonicPosition = +window
+        .getComputedStyle(sonic)
+        .bottom.replace('px', '');
+
+    if (
+        espinhoPosition <= 120 &&
+        espinhoPosition > 0 &&
+        sonicPosition < 150
+    ) {
+
+        /* PARAR ESPINHO */
+
+        espinho.style.animation = 'none';
+        espinho.style.left = `${espinhoPosition}px`;
+
+        /* PARAR SONIC */
+
+        sonic.style.animation = 'none';
+        sonic.style.bottom = `${sonicPosition}px`;
+
+        /* MORTE */
+
+        sonic.src = './imagem/sonic morrendo.webp';
+        sonic.style.width = '90px';
+        sonic.style.marginLeft = '50px';
+
+        clearInterval(loop);
+    }
+
+}, 10);
+
+document.addEventListener('keydown', jump);
